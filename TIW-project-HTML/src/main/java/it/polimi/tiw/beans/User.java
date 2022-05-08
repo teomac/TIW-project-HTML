93e3988bb9ed6ec6df9a5ef7c2b7021d6b5d4b86
magicteo <matty.macaluso@gmail.com>
2022-05-08 19:57:26 +0200

    Added user bean and userDAO and fixed loginServlet

---
+++ TIW-project-HTML/src/main/java/it/polimi/tiw/beans/User.java
@@ -0,0 +1,50 @@
+package it.polimi.tiw.controllers;
+
+public class User {
+	
+	private int id;
+	private String username;
+	private String name;
+	private String surname;
+	private String email;
+	
+	public getId() {
+		return id;
+	}
+	
+	public setId(int id) {
+		this.id = id;
+	}
+	
+	public getUsername() {
+		return username;
+	}
+	
+	public setUsername(String username){
+		this.username = username;
+	}
+	
+	public getName() {
+		return name;
+	}
+	
+	public setName(String name){
+		this.name = name;
+	}
+	
+	public getSurname() {
+		return surname;
+	}
+	
+	public setSurname(String surname){
+		this.surname = surname;
+	}
+	
+	public getEmail() {
+		return email;
+	}
+	
+	public setEmail(String email){
+		this.email = email;
+	}
+}
--- TIW-project-HTML/src/main/java/it/polimi/tiw/controllers/LoginServlet.java
+++ TIW-project-HTML/src/main/java/it/polimi/tiw/controllers/LoginServlet.java
@@ -13,35 +13,87 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+import org.apache.commons.lang.StringEscapeUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
+import it.polimi.tiw.beans.User;
+import it.polimi.tiw.dao.UserDAO;
+import it.polimi.tiw.utils.ConnectionHandler;
 
 @WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet{
 	private final static long serialVersionUID = 1L;
 	
 	private Connection connection = null;
+	
+	public LoginServlet() {
+		super();
+	}
+	
+	public void init() throws ServletException {
+		connection = ConnectionHandler.getConnection(getServletContext());
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		
 			String path = null;
-		
-		
-			String username = request.getParameter("username");
-			String password = request.getParameter("password");
 			
-			if(username==null || password == null) {
+			try {
+				String username = request.getParameter("username");
+				String password = request.getParameter("password");
+				
+			}catch (Exception e) {
 				response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Credenziali mancanti o incomplete");
+			}
+			
+			UserDAO userDAO = new UserDAO(connection);
+			User user = new User();
+			user = null;
+			
+			try {
+				user = userDAO.checkCredentials(username, password);
+			} catch (SQLException e) {
+				response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Non è stato possibile controllare le credenziali");
 				return;
 			}
 			
-			else{
-				
+			
+			if (user == null) {
+				ServletContext servletContext = getServletContext();
+				final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+				ctx.setVariable("errorMsg", "Username o password errati");
+				path = "/index.html";
+				templateEngine.process(path, ctx, response.getWriter());
+			} else {
+				request.getSession().setAttribute("user", user);
+				path = getServletContext().getContextPath() + "/Home";
 				response.sendRedirect(path);
 			}
 
-		
+		}
+			
+			
+			public void destroy() {
+				try {
+					ConnectionHandler.closeConnection(connection);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+			
+
 	}
 
 }
