610c5dadbcdc44fe4cd3a44ba99bad9d924118ad
magicteo <matty.macaluso@gmail.com>
2022-08-06 11:57:35 +0200

    Updated ProductDAO

--- TIW-project-HTML/src/main/java/it/polimi/tiw/projects/dao/ProductDAO.java
+++ TIW-project-HTML/src/main/java/it/polimi/tiw/projects/dao/ProductDAO.java
@@ -1,12 +1,39 @@
 package it.polimi.tiw.projects.dao;
 
+import it.polimi.tiw.projects.beans.*;
+
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDAO {
 	private Connection connection;
 	
 	public ProductDAO(Connection connection) {
 		this.connection = connection;
+	}
+	
+	public List<Product> list() throws SQLException{
+		List<Product> products = new ArrayList<>();
+		
+		String SQLQuery = "SELECT * FROM testdb.products";
+		
+		try(PreparedStatement statement = connection.prepareStatement(SQLQuery);
+				ResultSet resultSet = statement.executeQuery();
+				){
+			while(resultSet.next()) {
+				Product product = new Product(resultSet.getInt("productCode"), resultSet.getString("productName"), resultSet.getString("productImage"), resultSet.getString("productImage));
+				products.add(product);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return products;
 	}
 
 }
